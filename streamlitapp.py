861fe2741e7936f703b609243d5ad52001f1b0a7
LBrahmam1900 <132330704+LBrahmam1900@users.noreply.github.com>
2023-05-02 11:34:45 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,2 +1,6 @@
 import streamlit as sl
 sl.title('My Parents New Healthy Dinner')
+sl.header('Breakfast Menu')
+sl.text('Omega 3 & Blueberry Oatmeal')
+sl.text('Kale, Spinach & Rocket Smoothie')
+sl.test('Hard-Boiled Free-Range Egg')
